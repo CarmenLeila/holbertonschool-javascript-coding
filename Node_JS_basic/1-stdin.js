fe421929e8c0c898d3eddd9073c95fd967bd66e4
CarmenLeila <146730823+CarmenLeila@users.noreply.github.com>
2024-05-29 23:29:29 +0200

    Using Process stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,18 +1,11 @@
-function yourName() {
-  process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-  process.stdin.setEncoding('utf8');
+process.stdin.on('data', (data) => {
+  if (data !== null) {
+    process.stdout.write(`Your name is: ${data}`);
+  }
+});
 
-  process.stdin.on('data', (input) => {
-    const name = input.toString();
-    process.stdout.write(`Your name is: ${name}`);
-    process.stdout.write('This important software is now closing\n');
-    process.exit();
-  });
-}
-
-module.exports = yourName;
-
-if (require.main === module) {
-  yourName();
-}
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
