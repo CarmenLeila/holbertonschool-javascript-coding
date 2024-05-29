4816acfdf5f5b0caea305b6c51910b4ae2a26f14
CarmenLeila <146730823+CarmenLeila@users.noreply.github.com>
2024-05-29 23:32:23 +0200

    fixing bugs

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,24 +1,31 @@
 const express = require('express');
 const countStudents = require('./3-read_file_async');
 
+const db = process.argv[2];
+
 const app = express();
+const port = 1245;
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
+  res.end();
 });
-
 app.get('/students', (req, res) => {
-  countStudents('./database.csv')
-    .then((data) => {
-      res.send(`This is the list of our students\n${data}`);
+  res.write('This is the list of our students\n');
+  countStudents(db)
+    .then((result) => {
+      res.write(`${result.totalNumber}\n`);
+      res.write(`${result.CS}\n`);
+      res.write(`${result.SWE}`);
+      res.end();
     })
-    .catch((err) => {
-      res.send(err.message);
+    .catch(() => {
+      res.write('Cannot load the database');
+      res.end();
     });
 });
-
-app.listen(1245, () => {
-  console.log('Application est lancée sur le port 1245');
+app.listen(port, () => {
+  console.log(`Server running : ${port}`);
 });
 
 module.exports = app;
